6cb7ff2da490368822980bbbccb4cbd89303500d
Arvid Sätterkvist <arvidsat@kth.se>
2017-03-04 00:35:40 +0100

    update test_game

--- test_game/app.js
+++ test_game/app.js
@@ -17,14 +17,15 @@ function preload() {
 }
 
 function setup() {
-  megaman = new CBN.Sprite(100, 100, 100, 100, 'megaman');
+  megaman = new CBN.Sprite(30, 20, 100, 100, 'megaman');
   app.scene.add(megaman);
-  mario = new CBN.Sprite(300, 100, 100, 100, 'mario');
+  mario = new CBN.Sprite(20, 20, 100, 100, 'mario');
   app.scene.add(mario);
 }
 
 function update() {
-  mario.x += 1; // doesn't work
+  mario.x += 0.1;
+  megaman.y += 1;
 }
 
 var app = new CBN.Application();
